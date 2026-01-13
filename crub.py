d8f6b8e75d9e31e5d7adbe7ac3704998240c5295
Hoang_Phuc <pham.phuc.david@gmail.com>
2026-01-13 14:59:24 +0700

    add update and delete feature

--- crub.py
+++ crub.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from model import Employee
+from fastapi import HTTPException
 
 def create_employee(db: Session, employee: dict):
     new_employee = Employee(**employee)
@@ -10,3 +11,27 @@ def create_employee(db: Session, employee: dict):
 
 def get_all_employees(db: Session):
     return db.query(Employee).all()
+
+def change_department(db: Session, new_department: str, user_id: int, user_password: str):
+    employee = db.query(Employee).filter_by(id = user_id, password = user_password).first()
+
+    if not employee:
+        raise HTTPException(status_code=404, detail="NOT FOUND")
+    
+    employee.department = new_department
+    db.commit()
+    db.refresh(employee)
+    return employee
+
+def delete_employee(db: Session, user_id: int, user_password: str):
+    employee = db.query(Employee).filter_by(id= user_id, password= user_password).first()
+
+    if not employee:
+        raise HTTPException(status_code=404, detail="NOT FOUND")
+    
+    db.delete(employee)
+    db.commit()
+    return {
+        "message": "DELETED",
+        "employee": employee
+    }
--- main.py
+++ main.py
@@ -36,3 +36,24 @@ async def show_all_employees(
     db: Session = Depends(get_db)
 ):
     return crub.get_all_employees(db)
+
+
+@app.put("/employee/{user_id}/department")
+async def change_department(
+    *,
+    user_id: int = Path(...),
+    db: Session = Depends(get_db),
+    new_department : Departments,
+    user_password:str = Query(..., min_length=8)
+):
+    return crub.change_department(db, new_department.value, user_id, user_password)
+
+
+@app.put("/employee/{user_id}/department/delete")
+async def delete_employee(
+    *,
+    user_id: int = Path(...),
+    user_password: str = Query(..., min_length=8),
+    db: Session = Depends(get_db)
+):
+    return crub.delete_employee(db, user_id, user_password)
